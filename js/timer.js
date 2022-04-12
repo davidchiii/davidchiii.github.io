47b14fe4a6cca737ce9bd44df4bb73f2460c8e92
daebisu <changdavid52@gmail.com>
2022-04-12 14:58:15 -0400

    timer

---
+++ js/timer.js
@@ -0,0 +1,22 @@
+var date = new Date();
+var hour = date.getUTCHours();
+
+var directory = "../assets/images/background/";
+
+hour = 2;
+
+function checkTime(){
+    if(hour => 6 && hour < 11){
+        //set values to morning
+        document.getElementsByClassName("masthead")[0].setAttribute("style","linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.7) 75%, #000 100%), url(" + dir + "morning.jpg");
+    }
+    else if(hour >= 11 && hour < 17){
+        //set values to afternoon
+        document.getElementsByClassName("masthead")[0].setAttribute("style","linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.7) 75%, #000 100%), url(" + dir + "afternoon.jpg");
+    }
+    else if(hour >= 17 || (hour >= 0 && hour < 6)){
+        //set values to night
+        document.getElementsByClassName("masthead")[0].setAttribute("style","linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.7) 75%, #000 100%), url(" + dir + "night.jpg");
+    }
+}
+checkTime();
